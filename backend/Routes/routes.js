d29cae5ae7f1777b25bb7f93d4b4c992db018ab9
Vraj Patel <vrajpatel089@gmail.com>
2022-12-04 04:11:33 -0500

    Deleted Dummy Backtest

--- backend/Routes/routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { addEmployee } from '../Controller/emp-controller.js';
-
-const router = express.Router();
-
-router.post('/add', addEmployee)
-
-export default router
